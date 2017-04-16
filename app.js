2a4cbf5bac71dedd6e32bca914ac0eab07d030b3
Mehmet Fırat Koloğlu <mfkologlu@gmail.com>
2017-04-16 16:26:54 +0300

    View controller deleted. Routing added to index.html.

--- app.js
+++ app.js
@@ -7,17 +7,5 @@
             $locationProvider.hashPrefix('!');
             $routeProvider.otherwise({ redirectTo: '/writing_desk' });
         }])
-
-        .controller('ViewController', function () {
-            this.activeView = 1;
-
-            this.isActiveView = function (view) {
-                return this.activeView === view;
-            };
-
-            this.setActiveView = function (view) {
-                this.activeView = view;
-            }
-        });
 })();
 
