5d43c1c7452baad39cd2952aa4b48133640ccb1f
Mehmet Fırat Koloğlu <mfkologlu@gmail.com>
2017-04-16 18:25:07 +0300

    added placeholder books in draft drawer.

--- views/draft_drawer/draft_drawer.js
+++ views/draft_drawer/draft_drawer.js
@@ -9,5 +9,17 @@
         }])
 
         .controller('DraftDrawerController', [function () {
+            this.books = [
+                {
+                    name : "Clear Skies",
+                    synopsis : "A story about a man and his plane.",
+                    content : "The boy once looked at the bright skies above. He imagined himself flying..."
+                },
+                {
+                    name : "Fun Times",
+                    synopsis : "A guide on how to throw the best parties!",
+                    content : "So you wanna be the guy, the dude, the mega, the girl or whatever. Well, you can! And I am gonna teach you how..."
+                }
+            ];
         }]);
 })();
