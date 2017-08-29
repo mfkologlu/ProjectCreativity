42e0ea754e2d98cbfbd295233cbc794cfcb37683
mfkologlu <mfkologlu@gmail.com>
2017-08-29 17:11:12 +0300

    Book deletion unit test.

--- test/BookModelTests.js
+++ test/BookModelTests.js
@@ -1,25 +1,35 @@
 var assert = require('assert');
 var Book = require('../api/models/Book');
 
-var textExample='Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a';
+var textExample = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a';
 
 describe('Book Model test suite', function () {
+    var newBook = new Book({
+        authorId: '4582415587415',
+        title: 'Adventures of a mushroom',
+        storyType: 'short-story',
+        genre: 'Fantasy',
+        text: textExample
+    });
+
     it('Create a new book', function (done) {
-
-        var newBook = new Book({
-            authorId:'4582415587415',
-            title:'Adventures of a mushroom',
-            storyType:'short-story',
-            genre:'Fantasy',
-            text:textExample
-        });
-
         newBook.save().then(function () {
             assert(!newBook.isNew);
-            assert(newBook.genre==='Fantasy');
+            assert(newBook.genre === 'Fantasy');
             done();
         });
 
+    });
+
+    it('Delete a book by ID', function (done) {
+        newBook.save().then(function () {
+            Book.findOneAndRemove({ _id: newBook._id }).then(function () {
+                Book.findOne({ _id: newBook._id }).then(function (result) {
+                    assert(result === null);
+                    done();
+                });
+            });
+        });
     });
 
 });
